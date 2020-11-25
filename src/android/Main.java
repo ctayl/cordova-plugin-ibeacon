a88c4bdf8c328e0d06d1ddd18ef0fc018dbb2964
Christopher Berger <chrisb000@gmail.com>
2020-11-24 19:08:16 -0800

    Cleanup and error handling

--- src/android/Main.java
+++ src/android/Main.java
@@ -80,88 +80,91 @@ public class Main extends Application implements BootstrapNotifier {
         beaconManager.getBeaconParsers().add(new BeaconParser().
                 setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
         beaconManager.setDebug(true);
-
-        Notification.Builder builder = new Notification.Builder(this);
-        Context context = getApplicationContext();
-        String packageName = context.getPackageName();
-
         try {
+
+//            beaconManager.setBeaconSimulator(new TimedBeaconSimulator());
+//            ((TimedBeaconSimulator) beaconManager.getBeaconSimulator()).createTimedSimulatedBeacons();
+
+            Notification.Builder builder = new Notification.Builder(this);
+            Context context = getApplicationContext();
+            String packageName = context.getPackageName();
+
             PackageManager manager = getPackageManager();
             Resources resources = manager.getResourcesForApplication(packageName);
             int resId = resources.getIdentifier("icon", "drawable", packageName);
-
-            Drawable myDrawable = resources.getDrawable(resId);
-
             builder.setSmallIcon(resId);
+
+            builder.setContentTitle("Scanning for Beacons");
+            Intent intent = new Intent(this, MainCordovaActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(
+                    this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+            );
+            builder.setContentIntent(pendingIntent);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
+                        "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
+                channel.setDescription("My Notification Channel Description");
+                NotificationManager notificationManager = (NotificationManager) getSystemService(
+                        Context.NOTIFICATION_SERVICE);
+                notificationManager.createNotificationChannel(channel);
+                builder.setChannelId(channel.getId());
+            }
+            beaconManager.enableForegroundServiceScanning(builder.build(), 456);
+            beaconManager.setEnableScheduledScanJobs(false);
+            beaconManager.setBackgroundBetweenScanPeriod(0);
+            beaconManager.setBackgroundScanPeriod(1100);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        builder.setContentTitle("Scanning for Beacons");
-        Intent intent = new Intent(this, MainCordovaActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
-        );
-        builder.setContentIntent(pendingIntent);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
-                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
-            channel.setDescription("My Notification Channel Description");
-            NotificationManager notificationManager = (NotificationManager) getSystemService(
-                    Context.NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(channel);
-            builder.setChannelId(channel.getId());
-        }
-        beaconManager.enableForegroundServiceScanning(builder.build(), 456);
-        beaconManager.setEnableScheduledScanJobs(false);
-        beaconManager.setBackgroundBetweenScanPeriod(0);
-        beaconManager.setBackgroundScanPeriod(1100);
     }
 
     private void reinitialize() {
+        try {
+            List<Region> regionList = new ArrayList<Region>();
 
-        List<Region> regionList = new ArrayList<Region>();
+            regions = sharedpreferences.getStringSet("regions", new HashSet<String>());
 
-        regions = sharedpreferences.getStringSet("regions", new HashSet<String>());
+            Iterator value = regions.iterator();
+            while (value.hasNext()) {
+                String array1[] = value.next().toString().split(",");
 
-        Iterator value = regions.iterator();
-        while (value.hasNext()) {
-            String array1[]= value.next().toString().split(",");
+                int index = 0;
 
-            int index = 0;
+                String uniqueIdentifier = "";
+                Identifier identifier1 = null;
+                Identifier identifier2 = null;
+                Identifier identifier3 = null;
 
-            String uniqueIdentifier = "";
-            Identifier identifier1 = null;
-            Identifier identifier2 = null;
-            Identifier identifier3 = null;
-
-            for (String identifier: array1) {
-                Log.w(TAG, identifier);
-                switch (index) {
-                    case 0: {
-                        uniqueIdentifier = identifier;
-                        break;
+                for (String identifier : array1) {
+                    Log.w(TAG, identifier);
+                    switch (index) {
+                        case 0: {
+                            uniqueIdentifier = identifier;
+                            break;
+                        }
+                        case 1: {
+                            identifier1 = !identifier.equals("NULL") ? Identifier.parse(identifier) : null;
+                            break;
+                        }
+                        case 2: {
+                            identifier2 = !identifier.equals("NULL") ? Identifier.parse(identifier) : null;
+                            break;
+                        }
+                        case 3: {
+                            identifier3 = !identifier.equals("NULL") ? Identifier.parse(identifier) : null;
+                            break;
+                        }
                     }
-                    case 1: {
-                        identifier1 = !identifier.equals("NULL") ? Identifier.parse(identifier) : null;
-                        break;
-                    }
-                    case 2: {
-                        identifier2 = !identifier.equals("NULL") ? Identifier.parse(identifier) : null;
-                        break;
-                    }
-                    case 3: {
-                        identifier3 = !identifier.equals("NULL") ? Identifier.parse(identifier) : null;
-                        break;
-                    }
+                    index++;
                 }
-                index++;
+                Region newRegion = new Region(uniqueIdentifier, identifier1, identifier2, identifier3);
+                regionList.add(newRegion);
             }
-            Region newRegion = new Region(uniqueIdentifier + "_" + identifier1, identifier1, identifier2, identifier3);
-            regionList.add(newRegion);
-        }
 
-        regionBootstrap = new RegionBootstrap(this, regionList);
+            regionBootstrap = new RegionBootstrap(this, regionList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public String parseRegion(Region region) {
@@ -177,145 +180,164 @@ public class Main extends Application implements BootstrapNotifier {
 
 
     public void startMonitoringForRegion(Region region) {
-        Log.e(TAG, String.valueOf(region));
+        try {
+            String entry = parseRegion(region);
+            SharedPreferences.Editor editor = sharedpreferences.edit();
 
-        String entry = parseRegion(region);
+            regions = sharedpreferences.getStringSet("regions", new HashSet<String>());
 
-        SharedPreferences.Editor editor = sharedpreferences.edit();
+            if (!regions.contains(entry)) {
+                regions.add(entry);
+            }
 
-        regions = sharedpreferences.getStringSet("regions", new HashSet<String>());
-        if (!regions.contains(entry)) {
-            Log.e(TAG, "cached new region!");
-            regions.add(entry);
-        } else {
-            Log.e(TAG, "region already cached!");
-        }
+            editor.clear();
+            editor.putStringSet("regions", regions);
+            editor.commit();
 
-        editor.clear();
-        editor.putStringSet("regions", regions);
-        editor.commit();
-
-        if (regionBootstrap != null) {
-            regionBootstrap.addRegion(region);
-        } else {
-            regionBootstrap = new RegionBootstrap(this, region);
+            if (regionBootstrap != null) {
+                regionBootstrap.addRegion(region);
+            } else {
+                regionBootstrap = new RegionBootstrap(this, region);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     public void stopMonitoringForRegion(Region region) {
+        try {
+            SharedPreferences.Editor editor = sharedpreferences.edit();
 
-        SharedPreferences.Editor editor = sharedpreferences.edit();
+            String entry = parseRegion(region);
 
-        String entry = parseRegion(region);
+            regions = sharedpreferences.getStringSet("regions", new HashSet<String>());
+            if (regions.contains(entry)) {
+                Log.e(TAG, "removed region!");
+                regions.remove(entry);
+            }
 
-        regions = sharedpreferences.getStringSet("regions", new HashSet<String>());
-        if (regions.contains(entry)) {
-            Log.e(TAG, "removed region!");
-            regions.remove(entry);
-        }
+            editor.clear();
+            editor.putStringSet("regions", regions);
+            editor.commit();
 
-        editor.clear();
-        editor.putStringSet("regions", regions);
-        editor.commit();
-
-        if (regionBootstrap != null) {
-            regionBootstrap.removeRegion(region);
+            if (regionBootstrap != null) {
+                regionBootstrap.removeRegion(region);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     public void didEnterRegion(Region region) {
+        try {
+            MainCordovaActivity mainCordovaActivity =  MainCordovaActivity.getInstance();
 
-        MainCordovaActivity mainCordovaActivity =  MainCordovaActivity.getInstance();
-
-        if (mainCordovaActivity != null && _callbackContext != null) {
-            LocationManager.getInstance().dispatchMonitorState("didEnterRegion", 1, region, _callbackContext);
-        } else {
-            sendNotification("didEnterRegion", 1, region);
+            if (mainCordovaActivity != null && _callbackContext != null) {
+                LocationManager.getInstance().dispatchMonitorState("didEnterRegion", 1, region, _callbackContext);
+            } else {
+                sendNotification("didEnterRegion", 1, region);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 
     @Override
     public void didExitRegion(Region region) {
+        try {
+            MainCordovaActivity mainCordovaActivity =  MainCordovaActivity.getInstance();
 
-        MainCordovaActivity mainCordovaActivity =  MainCordovaActivity.getInstance();
-
-        if (mainCordovaActivity != null && _callbackContext != null) {
-            LocationManager.getInstance().dispatchMonitorState("didExitRegion", 0, region, _callbackContext);
+            if (mainCordovaActivity != null && _callbackContext != null) {
+                LocationManager.getInstance().dispatchMonitorState("didExitRegion", 0, region, _callbackContext);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
 
     @Override
     public void didDetermineStateForRegion(int state, Region region) {
-        Log.w(TAG, "Current region state is: " + (state == 1 ? "INSIDE" : "OUTSIDE ("+state+")"));
+        try {
+            MainCordovaActivity mainCordovaActivity =  MainCordovaActivity.getInstance();
 
-        MainCordovaActivity mainCordovaActivity =  MainCordovaActivity.getInstance();
-
-        if (mainCordovaActivity != null && _callbackContext != null) {
-            LocationManager.getInstance().dispatchMonitorState("didDetermineStateForRegion", state, region, _callbackContext);
+            if (mainCordovaActivity != null && _callbackContext != null) {
+                LocationManager.getInstance().dispatchMonitorState("didDetermineStateForRegion", state, region, _callbackContext);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
 
     private void sendNotification(String eventType, int state, Region region) {
-
-        NotificationManager notificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Notification.Builder builder;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("Beacon Reference Notifications",
-                    "Beacon Reference Notifications", NotificationManager.IMPORTANCE_HIGH);
-            channel.enableLights(true);
-            channel.enableVibration(true);
-            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-            notificationManager.createNotificationChannel(channel);
-            builder = new Notification.Builder(this, channel.getId());
-        } else {
-            builder = new Notification.Builder(this);
-            builder.setPriority(Notification.PRIORITY_HIGH);
-        }
-
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-
-        Context context = getApplicationContext();
-        String pkgName  = context.getPackageName();
-
-        Intent intent = context
-                .getPackageManager()
-                .getLaunchIntentForPackage(pkgName);
-
-        intent.addFlags(FLAG_ACTIVITY_REORDER_TO_FRONT | FLAG_ACTIVITY_SINGLE_TOP);
-
-        Log.w(TAG, String.valueOf(region));
-        intent.putExtra("eventType", eventType);
-        intent.putExtra("state", state);
-        intent.putExtra("mUniqueId", region.getUniqueId());
-        intent.putExtra("id1", region.getId1().toString());
-        intent.putExtra("id2", region.getId2());
-        intent.putExtra("id3", region.getId3());
-
-        stackBuilder.addNextIntent(intent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
         try {
-            PackageManager manager = getPackageManager();
-            Resources resources = manager.getResourcesForApplication(pkgName);
-            int resId = resources.getIdentifier("icon", "drawable", pkgName);
+            NotificationManager notificationManager =
+                    (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
-            builder.setSmallIcon(resId);
+            Notification.Builder builder;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel("Beacon Reference Notifications",
+                        "Beacon Reference Notifications", NotificationManager.IMPORTANCE_HIGH);
+                channel.enableLights(true);
+                channel.enableVibration(true);
+                channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+                notificationManager.createNotificationChannel(channel);
+                builder = new Notification.Builder(this, channel.getId());
+            } else {
+                builder = new Notification.Builder(this);
+                builder.setPriority(Notification.PRIORITY_HIGH);
+            }
+
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+            Context context = getApplicationContext();
+            String pkgName = context.getPackageName();
+
+            Intent intent = context
+                    .getPackageManager()
+                    .getLaunchIntentForPackage(pkgName);
+
+            intent.addFlags(FLAG_ACTIVITY_REORDER_TO_FRONT | FLAG_ACTIVITY_SINGLE_TOP);
+
+            intent.putExtra("eventType", eventType);
+            intent.putExtra("state", state);
+            intent.putExtra("mUniqueId", region.getUniqueId());
+            intent.putExtra("id1", region.getId1() != null ? region.getId1().toString() : null);
+            intent.putExtra("id2", region.getId2() != null ? region.getId2().toString() : null);
+            intent.putExtra("id3", region.getId3() != null ? region.getId3().toString() : null);
+
+            Log.e(TAG, eventType);
+            Log.e(TAG, String.valueOf(state));
+            Log.e(TAG, region.getUniqueId());
+            Log.e(TAG, region.getId1() != null ? region.getId1().toString() : "null");
+            Log.e(TAG, region.getId2() != null ? region.getId2().toString() : "null");
+            Log.e(TAG, region.getId3() != null ? region.getId3().toString() : "null");
+
+            stackBuilder.addNextIntent(intent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            try {
+                PackageManager manager = getPackageManager();
+                Resources resources = manager.getResourcesForApplication(pkgName);
+                int resId = resources.getIdentifier("icon", "drawable", pkgName);
+
+                builder.setSmallIcon(resId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            builder.setContentTitle("I detect a beacon");
+            builder.setContentText("Tap here to see the details");
+            builder.setContentIntent(resultPendingIntent);
+            notificationManager.notify(1, builder.build());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        builder.setContentTitle("I detect a beacon");
-        builder.setContentText("Tap here to see details in the reference app");
-        builder.setContentIntent(resultPendingIntent);
-        notificationManager.notify(1, builder.build());
     }
+
 }
--- src/android/MainCordovaActivity.java
+++ src/android/MainCordovaActivity.java
@@ -19,37 +19,14 @@
 
 package com.ibeaconbg.www;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import java.util.UUID;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-
-import com.unarin.cordova.beacon.LocationManager;
-
-import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.Identifier;
 import org.altbeacon.beacon.Region;
 import org.apache.cordova.*;
 
-import java.lang.ref.WeakReference;
-
-import de.appplant.cordova.plugin.localnotification.LocalNotification;
-
 public class MainCordovaActivity extends CordovaActivity
 {
-    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
-    private static final int PERMISSION_REQUEST_BACKGROUND_LOCATION = 2;
-    private static WeakReference<CordovaWebView> webView = null;
     private static MainCordovaActivity sInstance;
 
     private static String eventType;
@@ -64,61 +41,42 @@ public class MainCordovaActivity extends CordovaActivity
 
         // enable Cordova apps to be started in the background
         Bundle extras = getIntent().getExtras();
-        Log.w("MainCordovaActivity", String.valueOf(extras));
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
             moveTaskToBack(true);
         }
 
-        if (extras != null) {
-            eventType = extras.getString("eventType");
-            state = extras.getInt("state");
 
-            Log.w(TAG, String.valueOf(state));
+        try {
+            if (extras != null) {
+                eventType = extras.getString("eventType");
+                state = extras.getInt("state");
 
-            String mUniqueId = extras.getString("mUniqueId");
-            Identifier uuid = extras.getString("id1") instanceof String ? Identifier.parse(extras.getString("id1")) : null;
-            Identifier id2 = extras.getString("id2") instanceof String ? Identifier.parse(extras.getString("id2")) : null;
-            Identifier id3 = extras.getString("id3") instanceof String ? Identifier.parse(extras.getString("id3")) : null;
+                Log.e(TAG, String.valueOf(state));
 
-            parsedRegion = new Region(mUniqueId, uuid, id2, id3);
+                String mUniqueId = extras.getString("mUniqueId") instanceof String ? extras.getString("mUniqueId") : null;
+                Identifier uuid = extras.getString("id1") != null ? Identifier.parse(extras.getString("id1")) : null;
+                Identifier id2 = extras.getString("id2") != null ? Identifier.parse(extras.getString("id2")) : null;
+                Identifier id3 = extras.getString("id3") != null ? Identifier.parse(extras.getString("id3")) : null;
+
+                if (mUniqueId != null) {
+                    parsedRegion = new Region(mUniqueId, uuid, id2, id3);
+                } else {
+                    Log.e(TAG, "Invalid Region! Ignoring.");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Set by <content src="index.html" /> in config.xml
+            loadUrl(launchUrl);
         }
-
-        // Set by <content src="index.html" /> in config.xml
-        loadUrl(launchUrl);
-//
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-//                    == PackageManager.PERMISSION_GRANTED) {
-//
-//            } else {
-//                if (!this.shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
-//                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-//                            PERMISSION_REQUEST_FINE_LOCATION);
-//                }
-//                else {
-//                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                    builder.setTitle("Functionality limited");
-//                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons.  Please go to Settings -> Applications -> Permissions and grant location access to this app.");
-//                    builder.setPositiveButton(android.R.string.ok, null);
-//                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-//
-//                        @Override
-//                        public void onDismiss(DialogInterface dialog) {
-//                        }
-//
-//                    });
-//                    builder.show();
-//                }
-//
-//            }
-//        }
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        sInstance = null;
-    }
+   @Override
+   public void onDestroy() {
+       super.onDestroy();
+       sInstance = null;
+   }
 
     public static MainCordovaActivity getInstance() {
         return MainCordovaActivity.sInstance;
@@ -126,7 +84,6 @@ public class MainCordovaActivity extends CordovaActivity
 
     public static void onInit() {
         if (eventType != null && parsedRegion != null) {
-            Log.e(TAG, "CALLING ON INIT");
             Main.getInstance().didDetermineStateForRegion(state, parsedRegion);
         }
     }
