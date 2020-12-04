0ccf275e45b969ec293e909de92e00380d26b39e
Christopher Berger <chrisb000@gmail.com>
2020-12-03 16:39:00 -0800

    idsable isLocationEnabled on ios

--- src/ios/CDVLocationManager.h
+++ src/ios/CDVLocationManager.h
@@ -93,7 +93,6 @@ const int CDV_LOCATION_MANAGER_INPUT_PARSE_ERROR = 100;
 - (void)isMonitoringAvailableForClass:(CDVInvokedUrlCommand*)command;
 
 - (void)isBluetoothEnabled:(CDVInvokedUrlCommand*)command;
-- (void)isLocationEnabled:(CDVInvokedUrlCommand*)command;
 - (void)enableBluetooth:(CDVInvokedUrlCommand*)command;
 - (void)disableBluetooth:(CDVInvokedUrlCommand*)command;
 
